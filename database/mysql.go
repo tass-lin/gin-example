13bab0345daf3222fde2f70e1cb9036d5c54d4db
Tass <ienienien2001@gmail.com>
2020-05-27 14:02:51 +0800

    https://gorm.io/docs/generic_interface.html

--- database/mysql.go
+++ database/mysql.go
@@ -4,6 +4,7 @@ import (
 	"gin-example/configs"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
+	"time"
 )
 
 var Eloquent *gorm.DB
@@ -14,6 +15,16 @@ func init() {
 	mysqlConfig := configs.GetMysqlConfig()
 	var mysqlDsn = mysqlConfig["MYSQL_DSN"]
 	Eloquent, err = gorm.Open("mysql", mysqlDsn)
+
+	Eloquent.LogMode(false)
+	// SetMaxIdleConns sets the maximum number of connections in the idle connection pool.
+	Eloquent.DB().SetMaxIdleConns(10)
+
+	// SetMaxOpenConns sets the maximum number of open connections to the database.
+	Eloquent.DB().SetMaxOpenConns(100)
+
+	// SetConnMaxLifetime sets the maximum amount of time a connection may be reused.
+	Eloquent.DB().SetConnMaxLifetime(time.Hour)
 
 	if err != nil {
 		panic(err)
